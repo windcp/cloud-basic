406bf626fab602be7361d7a31555f9177d8477f2
v_vwlkunli <v_vwlkunli@tencent.com>
2022-10-27 16:34:58 +0800

    1,rabbitmq 投递消息 异步确认

--- cloud-base/service-rabbitmq/src/main/java/com/wind/rabbitmq/quenelistener/demotest/DemoTestReceiver.java
+++ cloud-base/service-rabbitmq/src/main/java/com/wind/rabbitmq/quenelistener/demotest/DemoTestReceiver.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Component;
 
--- cloud-base/service-rabbitmq/src/main/java/com/wind/rabbitmq/service/RabbitSendService.java
+++ cloud-base/service-rabbitmq/src/main/java/com/wind/rabbitmq/service/RabbitSendService.java
@@ -2,9 +2,16 @@ package com.wind.rabbitmq.service;
 
 import com.wind.rabbitmq.model.dto.MessageDto;
 import com.wind.rabbitmq.utils.DESUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +23,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitSendService {
 
+    private static Logger logger = LoggerFactory.getLogger(RabbitSendService.class);
+
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
@@ -25,7 +34,23 @@ public class RabbitSendService {
         String messageId = DESUtil.generateRandomKey(8);
         messageDto.setMessageId(messageId);
         messageDto.setCreateTime(System.currentTimeMillis());
-        rabbitTemplate.convertAndSend(exchangeName,bingKey,messageDto);
+//        rabbitTemplate.convertAndSend(exchangeName,bingKey,messageDto);
+
+        //异步确认是否消息被接收
+        CorrelationData cd1 = new CorrelationData();
+        cd1.setId(messageId);
+        rabbitTemplate.convertAndSend(exchangeName,bingKey,messageDto,cd1);
+        try {
+            if(cd1.getFuture().get(10, TimeUnit.SECONDS).isAck()){
+                logger.info("消息成功投递");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
         return messageDto.getMessageId();
     }
 
